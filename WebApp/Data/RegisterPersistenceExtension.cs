c963100d43e3d5e28f16e77b9350f9fd30a5eaee
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-01-30 10:34:55 +0100

    Added SecondLevelCacheInterceptor

--- WebApp/Data/RegisterPersistenceExtension.cs
+++ WebApp/Data/RegisterPersistenceExtension.cs
@@ -1,3 +1,4 @@
+using EFCoreSecondLevelCacheInterceptor;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Data;
@@ -6,8 +7,18 @@ public static class RegisterPersistenceExtension
 {
     public static IServiceCollection RegisterPersistence(this IServiceCollection services, string? connectionString)
     {
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(connectionString));
+        services.AddEFSecondLevelCache(options =>
+            {
+                options.UseMemoryCacheProvider().DisableLogging(true).UseCacheKeyPrefix("EF_");
+                options.CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(10));
+            }
+        );
+
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            options.UseNpgsql(connectionString)
+                .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>());
+        });
 
         return services;
     }
