c32d1074b423ff374f7e9d9639c4bf491b5224ce
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-02-15 15:23:19 +0100

    Made the submit feature

--- WebApp/ClientApp/src/components/joinedContestCard.tsx
+++ WebApp/ClientApp/src/components/joinedContestCard.tsx
@@ -1,13 +1,18 @@
 import "dayjs/locale/hu";
 
-import { ActionIcon, Box, Button, Card, Group, Text, Title, createStyles } from "@mantine/core";
+import { ActionIcon, Box, Button, Card, FileInput, Group, LoadingOverlay, Text, Title, createStyles } from "@mantine/core";
+import { IconCheck, IconChevronRight } from "@tabler/icons-react";
+import { IndexContestsResponse, PostApiContestEntriesContestIdBody } from "../api/client/model";
+import { closeAllModals, openConfirmModal, openModal } from "@mantine/modals";
+import { useDeleteApiContestEntriesContestIdOwn, useGetApiContestEntriesContestIdOwn, usePostApiContestEntriesContestId } from "../api/client/contest-entries/contest-entries";
+import { useEffect, useState } from "react";
 
-import { IconChevronRight } from "@tabler/icons-react";
-import { IndexContestsResponse } from "../api/client/model";
 import { Link } from "react-router-dom";
+import { ValidationError } from "../utils/api";
 import dayjs from "dayjs";
-import { openModal } from "@mantine/modals";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { showNotification } from "@mantine/notifications";
+import { useForm } from "@mantine/form";
 import { useGetApiContestsIdExercise } from "../api/client/contests/contests";
 
 dayjs.extend(relativeTime);
@@ -23,16 +28,83 @@ const useStyles = createStyles((theme) => ({
 
 const ViewJoinedContestModalContent = ({ contest }: { contest: IndexContestsResponse }): JSX.Element => {
     const downloadExercise = useGetApiContestsIdExercise(contest.id, { query: { enabled: false } });
+    const ownSolution = useGetApiContestEntriesContestIdOwn(contest.id, { query: { retry: false } });
+
+    const createSolution = usePostApiContestEntriesContestId();
+    const deleteSolution = useDeleteApiContestEntriesContestIdOwn();
 
     const downloadExerciseFile = () => {
         downloadExercise.refetch();
         downloadExercise.remove();
     };
 
+    const form = useForm({
+        initialValues: {
+            Solution: null
+        },
+    });
+
+    const submit = form.onSubmit(async (values) => {
+        try {
+            await createSolution.mutateAsync({
+                contestId: contest.id,
+                data: values as unknown as PostApiContestEntriesContestIdBody,
+            });
+            ownSolution.refetch();
+            showNotification({
+                title: "Sikeres beküldés",
+                color: "green",
+                icon: <IconCheck />,
+                message: "A megoldás sikeresen beküldve",
+            });
+        } catch (error) {
+            if (error instanceof ValidationError) {
+                handleValidationErrors(error);
+            }
+        }
+    });
+
+    const handleValidationErrors = (error: ValidationError) => {
+        for (const validationError in error.errors) {
+            let message = "";
+            for (const err of error.errors[validationError]) {
+                message += `${err}\n`;
+            }
+            form.setFieldError(validationError, message);
+        }
+    };
+
+    const openConfirmDeleteModal = async () => {
+        openConfirmModal({
+            title: "Biztosan törölni szeretnéd a megoldást?",
+            children: (
+                <Text size="sm">
+                Biztos, hogy törölni akarod a már beküldött megoldásadat? Ez a művelet nem visszavonható, de ha meg van még a fájl, akkor beküldheted újra.
+                </Text>
+            ),
+            labels: { confirm: "Megoldás törlése", cancel: "Mégse" },
+            onConfirm: async () => {
+                await deleteSolution.mutateAsync({ contestId: contest.id });
+                ownSolution.remove();
+                ownSolution.refetch();
+                closeAllModals();
+            },
+        });
+    };
+
     return (
         <>
-            <Text color="dimmed">{contest.description}</Text>
-            <Button fullWidth={true} onClick={downloadExerciseFile} mt="md">Feladat letöltése</Button>
+            <LoadingOverlay visible={ownSolution.isLoading} />
+            <Text color="dimmed" mb="sm">{contest.description}</Text>
+            <Button fullWidth={true} onClick={downloadExerciseFile} mb="sm">Feladat letöltése</Button>
+            {ownSolution.data ? (
+                <Button fullWidth={true} color="red" onClick={openConfirmDeleteModal}>Megoldás törlése</Button>
+            ) : (
+                <form onSubmit={submit}>
+                    <FileInput label="Megoldás" required={true} {...form.getInputProps("Solution")} mb="sm" />
+                    <Button fullWidth={true} type="submit">Beküldés</Button>
+                </form>
+            )}
         </>
     );
 };
