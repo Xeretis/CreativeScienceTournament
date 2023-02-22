849f3774cfbbd3fc7fb4fa9d6de613a08ed24d3f
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-02-22 17:22:11 +0100

    Made solutions downloadable

--- WebApp/ClientApp/src/components/contestEntryCard.tsx
+++ WebApp/ClientApp/src/components/contestEntryCard.tsx
@@ -1,25 +1,62 @@
-import { Box, Card, Group, Text, createStyles, useMantineTheme } from "@mantine/core";
+import { Box, Button, Card, Group, Text, createStyles, useMantineTheme } from "@mantine/core";
+import { IconCheck, IconChevronRight } from "@tabler/icons-react";
 
-import { IconChevronRight } from "@tabler/icons-react";
 import { IndexContestEntriesResponse } from "../api/client/model";
+import { openModal } from "@mantine/modals";
+import { useGetApiContestEntriesIdSolution } from "../api/client/contest-entries/contest-entries";
 
 const useStyles = createStyles((theme) => ({
+    card: {
+        cursor: "pointer",
+    },
     textContainer: {
         maxWidth: "72%",
     },
 }));
 
+const ViewContestEntryModalContent = ({ entry }: { entry: IndexContestEntriesResponse }): JSX.Element => {
+    const { classes } = useStyles();
+
+    const downloadSloution = useGetApiContestEntriesIdSolution(entry.id, { query: { enabled: false } });
+
+    const downloadSolutionFile = () => {
+        downloadSloution.refetch();
+        downloadSloution.remove();
+    };
+
+    return (
+        <>
+            <Group position="apart" mb="sm">
+                <Text>Csapat: </Text>
+                <Text weight={600}>{entry.team.name}</Text>
+            </Group>
+            <Button fullWidth={true} variant="outline" onClick={downloadSolutionFile}>Megoldás letöltése</Button>
+        </>
+    );
+};
+
 export const ContestEntryCard = ({ entry }: { entry: IndexContestEntriesResponse }): JSX.Element => {
     const { classes } = useStyles();
     const theme = useMantineTheme();
 
+    const openViewContestEntryModal = () => {
+        openModal({
+            title: "Megoldás megtekintése",
+            size: "lg",
+            children: <ViewContestEntryModalContent entry={entry} />,
+        });
+    };
+
     return (
-        <Card shadow="sm" p="md" radius="md" withBorder={theme.colorScheme !== "dark"}>
+        <Card onClick={openViewContestEntryModal} shadow="sm" p="md" radius="md" withBorder={theme.colorScheme !== "dark"} className={classes.card}>
             <Group position="apart">
                 <Box className={classes.textContainer}>
                     <Text size="xl" weight={600} truncate={true}>{entry.team.name}</Text>
                 </Box>
-                <IconChevronRight stroke={1.5} />
+                <Group>
+                    {entry.correction && <IconCheck stroke={1.5} />}
+                    <IconChevronRight stroke={1.5} />
+                </Group>
             </Group>
         </Card>
     );
